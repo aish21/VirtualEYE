852bb55d456e935452011fd27eae9fc97d38e0d2
Aishwarya Singh <aish.akshu@gmail.com>
2022-09-25 22:15:36 +0800

    Add numpy array tests

--- server/objDet.py
+++ server/objDet.py
@@ -1,17 +1,22 @@
 ''' Python Script for simple object detection tests '''
 
 from imageai.Detection import ObjectDetection
+from PIL import Image
+from numpy import asarray
 
 detector = ObjectDetection()
 
-model_path = "./models/resnet.h5"
-input_path = "./input/test2.jpeg"
-output_path = "./output/newimage_resnet.jpg"
+model_path = "./models/yolo.h5"
+# input_path = "./input/test2.jpeg"
+output_path = "./output/speed_yolo_5.jpg"
 
-detector.setModelTypeAsRetinaNet()
+img = Image.open('./input/test2.jpeg')
+numpydata = asarray(img)
+
+detector.setModelTypeAsYOLOv3()
 detector.setModelPath(model_path)
-detector.loadModel()
+detector.loadModel(detection_speed="fast")
 
-detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
+detection = detector.detectObjectsFromImage(input_type="array", input_image=numpydata, output_image_path=output_path, minimum_percentage_probability=75)
 for eachItem in detection:
     print(eachItem["name"] , " : ", eachItem["percentage_probability"])
