d8d41ecf70a21c274df26aeba518afade6a52bcd
Aishwarya Singh <aish.akshu@gmail.com>
2022-09-25 20:58:17 +0800

    clean up

--- server/objectDetection.py
+++
@@ -1,9 +0,0 @@
-''' Python Script for simple object detection tests '''
-
-import os
-from imageai.Detection import ObjectDetection
-
-execPath = os.getcwd()
-
-detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
