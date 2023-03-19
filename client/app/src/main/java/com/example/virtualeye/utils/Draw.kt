c506917c6a985c098805de27b7ec134674631ea9
aish21 <aish.akshu@gmail.com>
2023-03-19 14:56:32 +0800

    Add comments - Draw

--- client/app/src/main/java/com/example/virtualeye/utils/Draw.kt
+++ client/app/src/main/java/com/example/virtualeye/utils/Draw.kt
@@ -1,5 +1,6 @@
 package com.example.virtualeye.utils
 
+// Required Imports
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
@@ -7,6 +8,15 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import android.view.View
+
+/**
+ * A custom View class that draws a rectangular boundary and text in it.
+ * The boundary is drawn using a red paint and the text is drawn using a green paint.
+ *
+ * @param context the context of the View
+ * @param rect a Rect object that defines the dimensions of the boundary
+ * @param text the text to be displayed in the boundary
+ */
 
 @SuppressLint("ViewConstructor")
 class Draw(context: Context?, var rect: Rect, var text: String) : View(context) {
@@ -17,6 +27,10 @@ class Draw(context: Context?, var rect: Rect, var text: String) : View(context) 
     init {
         init()
     }
+
+    /**
+     * Initializes the boundary and text paints with appropriate colors, widths, and styles.
+     */
 
     private fun init() {
         boundaryPaint = Paint()
@@ -31,7 +45,14 @@ class Draw(context: Context?, var rect: Rect, var text: String) : View(context) 
         textPaint.textSize = 60f // Set the text size to 50 pixels
     }
 
+    /**
+     * Draws the boundary and text on the canvas.
+     *
+     * @param canvas the canvas on which to draw the boundary and text
+     */
+
     override fun onDraw(canvas: Canvas?) {
+
         // Calculate the width of the text
         val textWidth = textPaint.measureText(text)
 
@@ -41,6 +62,6 @@ class Draw(context: Context?, var rect: Rect, var text: String) : View(context) 
         // Draw the text and the box
         canvas?.drawText(text, textX, rect.centerY().toFloat(), textPaint)
         canvas?.drawRect(rect.left.toFloat(), rect.top.toFloat(), rect.bottom.toFloat(), rect.right.toFloat(), boundaryPaint)
-
     }
+    
 }
